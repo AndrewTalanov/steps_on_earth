87c408e8de4485d0c20f04d6009fa785cd6064a2
AndrewBavyka <abavyka@list.ru>
2022-05-22 19:56:12 +0500

    Добавлен контент в табах и анимация для периодов(надо как то ускорять)

--- src/js/files/periods.js
+++ src/js/files/periods.js
@@ -1,5 +1,103 @@
 import anime from 'animejs/lib/anime.es.js';
 
+let btnDrop = document.querySelectorAll('.dropdown-info');
+let contentDrop = document.querySelectorAll('.periods-items');
+
+btnDrop.forEach((item, id) => {
+    item.addEventListener('click', () => {
+
+        let showArrowContentAnimation = anime.timeline({
+            easing: 'easeOutExpo',
+            duration: 500,
+        });
+
+        const interval = 400;
+
+        if (contentDrop[id].style.opacity == 0) {
+            showArrowContentAnimation
+                .add({
+                    targets: '.dropdown-info',
+                    rotateX: 180,
+                    delay: 300,
+                })
+                .add({
+                    targets: contentDrop[id],
+                    opacity: 1,
+                    easing: 'easeInOutQuad',
+                })
+                .add({
+                    targets: '.group-wrapper',
+                    translateY: [-50, 0],
+                    opacity: [0, 1],
+                    easing: 'easeInOutQuad',
+                    duration: (el, i) => i * interval + interval,
+                });
+
+        } else if (contentDrop[id].style.opacity == 1) {
+            showArrowContentAnimation
+
+                .add({
+                    targets: '.dropdown-info',
+                    rotateX: 0,
+                })
+
+                .add({
+                    targets: contentDrop[id],
+                    opacity: 0,
+                    easing: 'easeInExpo',
+                })
+
+                .add({
+                    targets: '.group-wrapper',
+                    opacity: 0,
+                    easing: 'easeInOutQuad',
+
+                })
+
+                .add({
+                    targets: '.item-1',
+                    opacity: [0, 1],
+                    translateY: [-100, 0],
+                    easing: 'easeInExpo',
+                    duration: (el, i) => i * interval + interval,
+                })
+
+                .add({
+                    targets: '.item-2',
+                    opacity: [0, 1],
+                    translateY: [-100, 0],
+                    easing: 'easeInExpo',
+                    duration: (el, i) => i * interval + interval,
+                })
+
+                .add({
+                    targets: '.item-3',
+                    opacity: [0, 1],
+                    translateY: [-100, 0],
+                    easing: 'easeInExpo',
+                    duration: (el, i) => i * interval + interval,
+                    delay: 100,
+                })
+                
+                .add({
+                    targets: '.item-4',
+                    opacity: [0, 1],
+                    translateY: [-100, 0],
+                    easing: 'easeInExpo',
+                    duration: (el, i) => i * interval + interval,
+                })
+                .add({
+                    targets: '.item-5',
+                    opacity: [0, 1],
+                    translateY: [-100, 0],
+                    easing: 'easeInExpo',
+                    duration: (el, i) => i * interval + interval,
+                });
+        }
+    });
+});
+
+
 // const dropBtn = document.querySelectorAll('.dropdown-info');
 
 // dropBtn.forEach(function (item) {
@@ -12,114 +110,4 @@ import anime from 'animejs/lib/anime.es.js';
 //         currentBtn.classList.toggle('active');
 //         currentTab.classList.toggle('active');
 //     });
-// });
-
-let btnDrop = document.querySelectorAll('.dropdown-info');
-let contentDrop = document.querySelectorAll('.periods-items');
-
-btnDrop.forEach((item, id) => {
-    item.addEventListener('click', () => {
-
-        let showArrowContentAnimation = anime.timeline({
-            easing: 'easeOutExpo',
-            duration: 200,
-        });
-
-        if (contentDrop[id].style.opacity == 0) {
-            showArrowContentAnimation
-
-                .add({
-                    targets: '.dropdown-info',
-                    rotateX: 180,
-                    delay: 300,
-                })
-                .add({
-                    targets: contentDrop[id],
-                    opacity: 1,
-                    easing: 'easeInOutQuad'
-                })
-
-                .add({
-                    targets: '.item-1',
-                    easing: 'easeInOutQuad',
-                    translateY: 0,
-                })
-                .add({
-                    targets: '.item-2',
-                    easing: 'easeInOutQuad',
-                    translateY: 0,
-                })
-
-                .add({
-                    targets: '.item-2',
-                    easing: 'easeInOutQuad',
-                    translateY: 0,
-                })
-                .add({
-                    targets: '.item-3',
-                    easing: 'easeInOutQuad',
-                    translateY: 0,
-                })
-                .add({
-                    targets: '.item-4',
-                    easing: 'easeInOutQuad',
-                    translateY: 0,
-                })
-
-
-        } else if (contentDrop[id].style.opacity == 1) {
-            showArrowContentAnimation
-                .add({
-                    targets: '.dropdown-info',
-                    rotateX: 0,
-                })
-                .add({
-                    targets: contentDrop[id],
-                    opacity: 0,
-                    easing: 'easeInOutQuad',
-                    delay: 200
-                })
-
-                .add({
-                    targets: '.item-1',
-                    easing: 'easeInOutQuad',
-                    translateY: -140,
-                    opacity: [0, 1],
-                    direction: 'alternate',
-                }, )
-
-                .add({
-                    targets: '.item-2',
-                    easing: 'easeInOutQuad',
-                    translateY: -130,
-                    opacity: [0, 1],
-                    direction: 'alternate',
-                }, )
-
-                .add({
-                    targets: '.item-3',
-                    easing: 'easeInOutQuad',
-                    translateY: -110,
-                    opacity: [0, 1],
-                }, )
-
-                .add({
-                    targets: '.item-4',
-                    easing: 'easeInOutQuad',
-                    translateY: -90,
-                    opacity: [0, 1],
-                })
-        }
-
-    });
-});
-
-
-// document.querySelector(".dropdown-info").addEventListener("click", () => {
-//     var tl = anime.timeline({
-//         easing: 'easeOutExpo',
-//         duration: 500
-//     });
-
-
 // });
